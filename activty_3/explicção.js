e74d1b8c7f2eaf726ae79aa967473a10e311b043
HMendesMartins <henriquemendesmartins9@gmail.com>
2026-03-12 12:09:30 -0300

    AYARE
    explicação das gavetas

---
+++ activty_3/explicção.js
@@ -0,0 +1,11 @@
+let lista_compras= ["arroz","feijão","açucar","salmão"];
+lista_compras.forEach(item =>{
+    lista_compras.push("macarrão");
+    lista_compras.push("picoca doce");
+    console.log(item);
+});
+let cesta_de_frutas=["maçã","manga","uva","limão"];
+cesta_de_frutas.forEach(fruta=>{
+    cesta_de_frutas.push("tambaqui")
+    consolke.log(fruta);
+});
