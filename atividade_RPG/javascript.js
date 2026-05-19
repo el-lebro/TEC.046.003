4ffb71a4394d5957df2dbcb43531b21a8571f6fd
henrique <henriqueduartemartinscmd@gmail.com>
2026-05-19 12:17:26 -0300

    javascript

---
+++ atividade_RPG/javascript.js
@@ -0,0 +1,29 @@
+class personagem {
+    constructor(nome, titulo, hp, mana, energia) {
+        this.nome = nome
+        this.titulo = titulo
+        this.hp = hp
+        this.mana = mana
+        this.energia = energia
+    }
+}
+class habilidade {
+    constructor(id, nome, dano, custo, energia) {
+        this.id = id
+        this.nome = nome
+        this.dano = dano
+        this.custo = custo
+        this.energia = energia
+    }
+}
+let hero = new personagem("jake", "o sonhador", 100, 100, 0);
+let boss = new personagem("litch", "o tenebre", 100, 0, 50);
+document.getElementById("nome-hero").textContent = hero.nome;
+document.getElementById("titulo-hero").textContent = hero.titulo
+document.getElementById("nome-boss").textContent = boss.nome;
+document.getElementById("titulo-boss").textContent = boss.titulo
+let listahabilidades = [
+    new habilidade(1, "🗡️ataque", 4, 0, 0)
+    new habilidade(2"☀️skill", 8, 10, 0)
+    new habilidade(3"🦑especial"15, 0, 100)
+];
